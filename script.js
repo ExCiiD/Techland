38e9e1e1b2ed786421d7f831b1cecaedf582ac79
ExCiiD <132538296+ExCiiD@users.noreply.github.com>
2023-05-17 15:38:04 +0400

    MAJ V2

--- script.js
+++ script.js
@@ -14,29 +14,29 @@ $('input').on('change', function() {
     $('body').toggleClass('blue');
   });
   function openSectionPortable() {
-    document.querySelector ('.sectionPortable').classList.remove('letsgo1');
-    document.querySelector ('.sectionPeripherique').classList.remove('letsgo1');
-    document.querySelector ('.sectionTourPC').classList.remove('letsgo2');
-    document.querySelector ('.sectionEcranPC').classList.remove('letsgo3');   
+    document.getElementById ('sectionPortable').classList.remove('letsgo1');
+    document.getElementById ('sectionPeripherique').classList.remove('letsgo1');
+    document.getElementById ('sectionTourPC').classList.remove('letsgo2');
+    document.getElementById ('sectionEcranPC').classList.remove('letsgo3');   
 }
 
 function openSectionPeripherique() {
-    document.querySelector ('.sectionPortable').classList.add('letsgo1');
-    document.querySelector ('.sectionPeripherique').classList.add('letsgo1');
-    document.querySelector ('.sectionTourPC').classList.remove('letsgo2');
-    document.querySelector ('.sectionEcranPC').classList.remove('letsgo3'); 
+    document.getElementById ('sectionPortable').classList.add('letsgo1');
+    document.getElementById ('sectionPeripherique').classList.add('letsgo1');
+    document.getElementById ('sectionTourPC').classList.remove('letsgo2');
+    document.getElementById ('sectionEcranPC').classList.remove('letsgo3'); 
 }
 
 function openSectionTourPC() {
-    document.querySelector ('.sectionPortable').classList.add('letsgo1');
-    document.querySelector ('.sectionPeripherique').classList.remove('letsgo1');
-    document.querySelector ('.sectionTourPC').classList.add('letsgo2');
-    document.querySelector ('.sectionEcranPC').classList.remove('letsgo3');  
+    document.getElementById ('sectionPortable').classList.add('letsgo1');
+    document.getElementById ('sectionPeripherique').classList.remove('letsgo1');
+    document.getElementById ('sectionTourPC').classList.add('letsgo2');
+    document.getElementById ('sectionEcranPC').classList.remove('letsgo3');  
 }
 
 function openSectionEcranPc() {
-    document.querySelector ('.sectionPortable').classList.add('letsgo1');
-    document.querySelector ('.sectionPeripherique').classList.remove('letsgo1');
-    document.querySelector ('.sectionTourPC').classList.remove('letsgo2');
-    document.querySelector ('.sectionEcranPC').classList.add('letsgo3'); 
+    document.getElementById ('sectionPortable').classList.add('letsgo1');
+    document.getElementById ('sectionPeripherique').classList.remove('letsgo1');
+    document.getElementById ('sectionTourPC').classList.remove('letsgo2');
+    document.getElementById ('sectionEcranPC').classList.add('letsgo3'); 
 }
